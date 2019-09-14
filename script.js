e03b92aac2cd43096a36b6833ce072f1eaca33a7
Karren Rellora <kprellora@gmail.com>
2019-09-14 23:36:34 +0930

    Coded the routes view

--- script.js
+++ script.js
@@ -3,57 +3,130 @@ fetch("bus_data.json")
   return data.json();
 })
 .then(function(bus_data) {
-  listAllBuses(bus_data);
+  createViews(bus_data);
 });
 
-var bus_list_button = document.getElementById("bus-nav");
+var bus_list_button = document.getElementsByClassName("nav-item")[0];
 bus_list_button.addEventListener("click", function() {
-  buses_page.style.display = "Block";
-  timetables_page.style.display = "None";
-  document.getElementById("bus-nav").classList.add("active-nav");
+  showBuses();
+  document.getElementsByClassName("nav-item")[0].classList.add("active-nav");
+  document.getElementsByClassName("nav-item")[1].classList.remove("active-nav");
+});
 
+var route_list_button = document.getElementsByClassName("nav-item")[1];
+route_list_button.addEventListener("click", function() {
+  showRoutes();
+  document.getElementsByClassName("nav-item")[0].classList.remove("active-nav");
+  document.getElementsByClassName("nav-item")[1].classList.add("active-nav");
 });
 
 var bus_container = document.getElementsByClassName("bus-container")[0];
+var route_container = document.getElementsByClassName("route-container")[0];
 var buses_page = document.getElementById("buses-page");
 var timetables_page = document.getElementById("timetables-page");
-timetables_page.style.display = "None";
+var routes_page = document.getElementById("routes-page");
+var routes = [
+  'Casuarina to Darwin and return',
+  'Casuarina to Palmerston and return',
+  'Casuarina to northern suburbs and return',
+  'Darwin to Palmerston and return',
+  'Darwin to Darwin suburbs and return',
+  'Palmerston to Palmerston suburbs and return',
+  'Rural areas to Palmerston and return',
+  'Mindil Beach sunset market'
+];
+var route_names = document.getElementsByClassName("route-name");
 
-// create a button for each bus
-function listAllBuses(bus_data) {
-  
+// create routes_page
+function createRoutesPage(bus_data) {
+  var routes_header = document.createElement("h2");
+  routes_header.innerHTML = "Routes";
+  var routes_topbar = document.getElementsByClassName("top-bar")[1];
+  routes_page.appendChild(routes_topbar);
+  routes_topbar.appendChild(routes_header);
+
   bus_data.forEach(function(bus) {
-    document.getElementById("bus-nav").classList.add("active-nav");
-    var button = document.createElement("div");
-    button.innerHTML = bus.bus;
-    button.classList.add("bus-button");
-    bus_container.appendChild(button);
+    var trip_name = bus["route"];
+      switch (trip_name) {
+        case 'Casuarina to Darwin and return':
+          createBusButton(bus, "route", 0);
+          break;
+        case 'Casuarina to Palmerston and return':
+          createBusButton(bus, "route", 1);
+          break;
+        case 'Casuarina to northern suburbs and return':
+          createBusButton(bus, "route", 2);
+          break;
+        case 'Darwin to Palmerston and return':
+          createBusButton(bus, "route", 3);
+          break;
+        case 'Darwin to Darwin suburbs and return':
+          createBusButton(bus, "route", 4);
+          break;
+        case 'Palmerston to Palmerston suburbs and return':
+          createBusButton(bus, "route", 5);
+          break;
+        case 'Rural areas to Palmerston and return':
+          createBusButton(bus, "route", 6);
+          break;
+        case 'Mindil Beach sunset market':
+          createBusButton(bus, "route", 7);
+          break;
+        default:
+          break;
+      }
+  });
+}
 
-    // set each button to create its timetable
-    button.addEventListener("click", function() {
-      buses_page.style.display = "None";
-      timetables_page.style.display = "Block";
-      createTimetablePage(bus);
-    });
+// create buses_page
+function createBusesPage(bus_data) {
+  var buses_header = document.createElement("h2");
+  buses_header.innerHTML = "Buses";
+  var buses_topbar = document.getElementsByClassName("top-bar")[0];
+  buses_topbar.appendChild(buses_header);
+  document.getElementsByClassName("nav-item")[0].classList.add("active-nav");
+
+  bus_data.forEach(function(bus) {
+    createBusButton(bus, "bus");
+  });
+}
+
+function createBusButton(bus, view, index) {
+  var button = document.createElement("div");
+  button.innerHTML = bus.bus;
+  button.classList.add("bus-button");
+  switch (view) {
+    case 'bus':
+      (bus_container).appendChild(button);
+      break;
+    case 'route':
+      (route_names)[index].appendChild(button);
+  }
+
+  // set each button to create its timetable
+  button.addEventListener("click", function() {
+    buses_page.style.display = "None";
+    routes_page.style.display = "None";
+    timetables_page.style.display = "Block";
+    createTimetablesPage(bus);
   });
 }
 
 // Code from https://www.valentinog.com/blog/html-table/
-function createTimetablePage(bus, trip = 0) { // using bus data
+function createTimetablesPage(bus, trip = 0) { // using bus data
   // reset timetable-container everytime a bus button is clicked
+  showTimetables();
   document.getElementById("bus-nav").classList.remove("active-nav");
   document.getElementById("timetable-container").remove();
 
   // create divs, set ID, append
   var timetable_container = document.createElement("div");
-  var info_container = document.createElement("div");
   var route_info = document.createElement("div");
   var route_switcher = document.createElement("div");
   var route_desc = document.createElement("div");
   var name = document.createElement("div");
   var trip_container = document.createElement("div");
   timetable_container.id = "timetable-container";
-  info_container.id = "info-container";
   route_info.id = "route-info";
   route_switcher.id = "route-switcher";
   route_desc.id = "route-desc";
@@ -62,10 +135,9 @@ function createTimetablePage(bus, trip = 0) { // using bus data
   name.classList.add("routing", "rs-name");
   trip_container.id = "trip-container";
   timetables_page.appendChild(timetable_container);
-  timetable_container.appendChild(info_container);
-  info_container.appendChild(route_info);
-  route_info.appendChild(route_switcher);
+  timetable_container.appendChild(route_info);
   route_info.appendChild(route_desc);
+  route_info.appendChild(route_switcher);
   route_switcher.appendChild(name);
   timetable_container.appendChild(trip_container);
 
@@ -82,17 +154,19 @@ function createTimetablePage(bus, trip = 0) { // using bus data
     var switch_button = document.createElement("div");
     switch_button.classList.add("rs-switcher");
     switch_button.innerHTML = "&#x21c4";
+
     // add button to switch div in route_info div
     switcher.appendChild(switch_button);
 
     // switch button changes trip value to the other one
     switch_button.addEventListener("click", function() {
       if (trip == 0) {
-        createTimetablePage(bus, trip = 1);
+        createTimetablesPage(bus, trip = 1);
       } else {
-        createTimetablePage(bus, trip = 0);
+        createTimetablesPage(bus, trip = 0);
       }
     });
+
   // if bus has only one trip (e.g. C <-> C)
   } else {
     // just display the trip name in route_info
@@ -103,7 +177,8 @@ function createTimetablePage(bus, trip = 0) { // using bus data
   var trip_desc = document.createElement("div");
   var bus_number = document.createElement("div");
   route_desc.appendChild(bus_number);
-  route_desc.appendChild(trip_desc);
+  // route_desc.appendChild(trip_desc);
+  route_desc.appendChild(route_switcher);
   trip_desc.innerHTML = bus["trips"][trip]["desc"];
   trip_desc.classList.add("trip-desc");
   bus_number.classList.add("bus-number");
@@ -133,7 +208,6 @@ function createTimetablePage(bus, trip = 0) { // using bus data
 
       var table = document.createElement("table");
       var data = Object.keys(timetable[0]);
-      // console.log(table_div.id);
       if (j > 0) {
         table_div.classList.add("hide");
       }
@@ -203,4 +277,29 @@ function createTableBody(table, data) {
       cell.appendChild(text);
     }
   }
+}
+
+// show pages
+function showBuses(){
+  buses_page.style.display = "Block";
+  timetables_page.style.display = "None";
+  routes_page.style.display = "None";
+}
+
+function showRoutes(){
+  routes_page.style.display = "Block";
+  timetables_page.style.display = "None";
+  buses_page.style.display = "None";
+}
+
+function showTimetables(){
+  timetables_page.style.display = "Block";
+  buses_page.style.display = "None";
+  routes_page.style.display = "None";
+}
+
+function createViews(bus_data) {
+  createBusesPage(bus_data);
+  createRoutesPage(bus_data);
+  showBuses();
 }
